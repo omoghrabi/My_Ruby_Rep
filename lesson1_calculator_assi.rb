48c61f0273b410d2b251b0ccf9e3eb7fe1bbfd90
Omar Moghrabi <Omarmoghrabi@Omars-MacBook-Pro.local>
2014-12-24 18:47:28 +0200

    lesson1_calculator_assi_v2

--- lesson1_calculator_assi.rb
+++ lesson1_calculator_assi.rb
@@ -1,7 +1,17 @@
 status = "c"
-while status!= "s" do
+while status != "s" do
   puts "please input your 1st number"
-  num1=gets.chomp.to_f
+
+  first_number = gets.chomp
+
+  while !first_number == first_number.to_i.to_s || first_number.to_f.to_s do
+    puts "your input is not an interger nor a float"
+    
+    puts "please input your 1st number"
+
+    first_number = gets.chomp!
+
+  end
 
   puts "please select an operation number from the following list
         1) Add
@@ -9,36 +19,60 @@ while status!= "s" do
         3) Multiply
         4) Divide"
 
-operation=gets.chomp
+  operation = gets.chomp
 
-puts "please input your 2nd number"
-  num2=gets.chomp.to_f
+  while !["1","2","3","4"].include? (operation) do
 
+    puts "Your input is not valid" 
 
-case operation
+    puts "please select an operation number from the following list
+          1) Add
+          2) Subtract
+          3) Multiply
+          4) Divide"
+    operation = gets.chomp
+
+  end
+
+  puts "please input your 2nd number"
+  
+  second_number = gets.chomp.to_f
+  
+  while !first_number == second_number.to_i.to_s || second_number.to_f.to_s do
+
+    puts "your input is not an interger nor a float"
+    
+    puts "please input your 2nd number"
+
+    first_number = gets.chomp!
+
+  end
+
+  case operation
 
   when "1"
 
-  puts num1 + num2
-
+    puts "the sum of #{first_number} and #{second_number} is #{first_number + second_number}"
 
   when "2"
 
-  puts num1 - num2
-
+    puts "the subtraction of #{second_number} from #{first_number} is #{first_number - second_number}"
 
   when "3"
 
-  puts num1 * num2
-
+    puts "the multiplication of #{first_number} by #{second_number} is #{first_number * second_number}"
 
   when "4"
+    if second_number == 0
+      puts "Error: can not divide by zero"
+    else
+      puts "the division of #{first_number} by #{second_number} is #{first_number / second_number}"
+    end 
 
-  puts num1 / num2
+  end 
 
-end 
+  puts "If you wish to exit the calculator enter s otherwise enter c"
 
-puts "If you wish to exit the calculator enter s otherwise enter c"
+  status = gets.chomp.downcase
 
-status=gets.chomp.undercase 
 end 
