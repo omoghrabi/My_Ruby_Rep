2c6bff44345317504e9f38002b81e0631f534354
Omar Moghrabi <Omarmoghrabi@Omars-MacBook-Pro.local>
2014-12-27 10:48:25 +0200

    blackjack_v2

--- lesson1_blackjack_assi.rb
+++ lesson1_blackjack_assi.rb
@@ -1,41 +1,69 @@
 #black jack game
 
 puts "Welcome to the Blackjack game"
-
-card_deck = {"1":1,"2":2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,"Jack":10,"Queen":10,"King":10,"Ace":11}
-
+card_deck = {1=>1,2=>2,3=>3,4=>4,5=>5,6=>6,7=>7,8=>8,9=>9,10=>10,"Jack"=>10,"Queen"=>10,"King"=>10,"Ace"=>11}
+player_hand = []
+dealer_hand = []
 begin 
   #palyer initial two cards 
-  player_first_card = card_deck.keys.sample().to_sym
-  player_second_card = card_deck.keys.sample().to_sym
-  puts "your first two cards are #{player_first_card} and #{player_second_card}"
+  player_first_card = card_deck.keys.sample()
+  player_hand << player_first_card
+  player_second_card = card_deck.keys.sample()
+  player_hand << player_second_card
+  puts "your first two cards are #{player_hand}"
   player_cards_sum = card_deck[player_first_card] + card_deck[player_second_card]
   puts "your total is #{player_cards_sum}"
   #dealer initial two cards
-  dealer_first_card = card_deck.keys.sample().to_sym
-  dealer_second_card = card_deck.keys.sample().to_sym
-  puts "dealer first two cards are #{dealer_first_card} and #{dealer_second_card}"
+  dealer_first_card = card_deck.keys.sample()
+  dealer_hand << dealer_first_card
+  dealer_second_card = card_deck.keys.sample()
+  dealer_hand << dealer_second_card
+  puts "dealer first two cards are #{dealer_hand}"
   dealer_cards_sum = card_deck[dealer_first_card] + card_deck[dealer_second_card]
   puts "dealer total is #{dealer_cards_sum}"
-  #player choice "hit" or "stay"
-  player_chocie == "hit"
-  while player_total <= 21 && player_chocie == "hit"
+  #player choice "hit" or "stay" and card output 
+  player_choice = "hit"
+  while player_cards_sum < 21 && player_choice == "hit" 
     puts "Do you want to Hit or Stay"
-    player_chocie = gets.chomp.downcase
-    unless player_chocie == "hit" || "stay" do
+    player_choice = gets.chomp.downcase
+    unless player_choice == "hit" || "stay" 
       puts "your input is not valid, please select either hit or stay"
-      player_chocie! = gets.chomp
+      player_choice = gets.chomp!
     end 
-    player_hit_card = card_deck.keys.sample().to_sym
-    player_total = player_total + card_deck[player_hit_card]
-    puts "your total is #{player_total}"
+    if player_choice == "hit"
+      player_hit_card = card_deck.keys.sample()
+      player_hand << player_hit_card
+      player_cards_sum = player_cards_sum + card_deck[player_hit_card]
+    end 
+    # if player_cards_sum >= 21 && player_hand.include? ("Ace")
+    #   player_cards_sum = player_cards_sum - player_hand.count ("Ace") * 10 
+    #end
+    puts "your hand is #{player_hand}"
+    puts "your total is #{player_cards_sum}"
   end 
-  while dealer_total <= 21 
-    dealer_hit_card = card_deck.keys.sample().to_sym
-    dealer_total = dealer_total + card_deck[dealer_hit_card]
-    puts "dealer total is #{dealer_total}"
+  #dealer card output
+  while dealer_cards_sum < 17 
+    dealer_hit_card = card_deck.keys.sample()
+    dealer_hand << dealer_hit_card
+    dealer_cards_sum = dealer_cards_sum + card_deck[dealer_hit_card]
+    puts "dealers hand is #{dealer_hand}"
+    puts "dealer total is #{dealer_cards_sum}"
   end
+  #compare dealer and player card cands 
+  if player_cards_sum == 21 && dealer_cards_sum != 21 
+    puts "Blackjack, you won!"
+  elsif player_cards_sum ==21 && dealer_cards_sum == 21 
+    puts "Tie"
+  elsif player_cards_sum > dealer_cards_sum && player_cards_sum < 21 
+    puts "You Won!"
+  elsif player_cards_sum > 21
+    puts "You lost!"
+  elsif player_cards_sum < dealer_cards_sum && dealer_cards_sum <= 21
+    puts "you lost!"
+  elsif player_cards_sum < dealer_cards_sum && dealer_cards_sum > 21
+    puts "you Won!"
+  end 
 
+  play_again = "n"
 
-
-end while paly_again != "n"
+end while play_again != "n"
