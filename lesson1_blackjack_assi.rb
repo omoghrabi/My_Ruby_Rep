95ea6f20f06fe38e29332f144961bb00d82a1089
Omar Moghrabi <Omarmoghrabi@Omars-MacBook-Pro.local>
2014-12-28 09:20:40 +0200

    blackjack_v3

--- lesson1_blackjack_assi.rb
+++ lesson1_blackjack_assi.rb
@@ -2,21 +2,21 @@
 
 puts "Welcome to the Blackjack game"
 card_deck = {1=>1,2=>2,3=>3,4=>4,5=>5,6=>6,7=>7,8=>8,9=>9,10=>10,"Jack"=>10,"Queen"=>10,"King"=>10,"Ace"=>11}
-player_hand = []
-dealer_hand = []
 begin 
+  player_hand = []
+  dealer_hand = []
   #palyer initial two cards 
-  player_first_card = card_deck.keys.sample()
+  player_first_card = card_deck.keys.sample
   player_hand << player_first_card
-  player_second_card = card_deck.keys.sample()
+  player_second_card = card_deck.keys.sample
   player_hand << player_second_card
   puts "your first two cards are #{player_hand}"
   player_cards_sum = card_deck[player_first_card] + card_deck[player_second_card]
   puts "your total is #{player_cards_sum}"
   #dealer initial two cards
-  dealer_first_card = card_deck.keys.sample()
+  dealer_first_card = card_deck.keys.sample
   dealer_hand << dealer_first_card
-  dealer_second_card = card_deck.keys.sample()
+  dealer_second_card = card_deck.keys.sample
   dealer_hand << dealer_second_card
   puts "dealer first two cards are #{dealer_hand}"
   dealer_cards_sum = card_deck[dealer_first_card] + card_deck[dealer_second_card]
@@ -26,18 +26,21 @@ begin
   while player_cards_sum < 21 && player_choice == "hit" 
     puts "Do you want to Hit or Stay"
     player_choice = gets.chomp.downcase
+
     unless player_choice == "hit" || "stay" 
       puts "your input is not valid, please select either hit or stay"
       player_choice = gets.chomp!
     end 
+
     if player_choice == "hit"
-      player_hit_card = card_deck.keys.sample()
+      player_hit_card = card_deck.keys.sample
       player_hand << player_hit_card
       player_cards_sum = player_cards_sum + card_deck[player_hit_card]
     end 
-    # if player_cards_sum >= 21 && player_hand.include? ("Ace")
-    #   player_cards_sum = player_cards_sum - player_hand.count ("Ace") * 10 
-    #end
+
+    if player_cards_sum > 21 && player_hand.include? ("Ace")
+      player_cards_sum = player_cards_sum - 10 
+    end
     puts "your hand is #{player_hand}"
     puts "your total is #{player_cards_sum}"
   end 
@@ -64,6 +67,7 @@ begin
     puts "you Won!"
   end 
 
-  play_again = "n"
+  puts "Do you want to play again (Y/N)"
+  play_again = gets.chomp.downcase
 
 end while play_again != "n"
