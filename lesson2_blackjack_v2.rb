2255dad91b37b6042c81981e6e80a63f613e87ee
Omar Moghrabi <Omarmoghrabi@Omars-MacBook-Pro.local>
2015-01-01 12:54:32 +0200

    blackjack_v3

--- lesson2_blackjack_v2.rb
+++ lesson2_blackjack_v2.rb
@@ -3,6 +3,7 @@ module Cards
   def card_selection
     @@card_deck.keys.sample
   end 
+
   def card_value(card)
     @@card_deck[card]
   end
@@ -10,53 +11,24 @@ end
 
 class Player
   include Cards
-  def initial_cards(player_hand,player_name)
-    2.times{player_hand << card_selection}
-    return "#{player_name}'s hand is #{player_hand}"
+  attr_accessor :player_hand, :player_total_value, :player_name, :selection
+  def initialize(player_name)
+    @player_name = player_name
+    @player_hand = []
+    @player_total_value = 0
+    @selection = ""
   end 
-  def value(player_total_value,player_hand,player_name)
-    player_hand.select{|x| player_total_value = player_total_value + card_value(x)}
-    return "#{player_name}'s hand value is #{player_total_value}"
+
+  def initial_cards
+    2.times{self.player_hand << card_selection}
+    "#{player_name}'s hand is #{player_hand}"
+  end 
+
+  def value
+    player_hand.select{|x| self.player_total_value = player_total_value + card_value(x)}
+    "#{player_name}'s hand value is #{player_total_value}"
   end
-end 
 
-class Dealer
-  include Cards
-  def initial_cards(dealer_hand)
-    2.times{dealer_hand << card_selection}
-    return "Dealer's hand is #{dealer_hand}"
-  end 
-  def value(dealer_total_value,dealer_hand)
-    dealer_hand.select{|x| dealer_total_value = dealer_total_value + card_value(x)}
-    return "Dealers's hand value is #{dealer_total_value}"
-  end 
-end 
-
-class BlackJack 
-  include Cards 
-  attr_accessor :player_hand, :player_total_value, :player_name, :player_status, :dealer_hand, :dealer_total_value, :selection 
-  def initialize
-  @player_name = ""
-  @player_hand = []
-  @player_total_value = 0
-  @dealer_hand = []
-  @dealer_total_value = 0
-  @selection = ""
-  end 
-  def start
-    puts "Please enter your name"
-    self.player_name = gets.chomp
-    player_one = Player.new
-    dealer = Dealer.new
-    puts player_one.initial_cards(player_hand,player_name)
-    puts player_one.value(player_total_value,player_hand,player_name)
-    puts dealer.initial_cards(dealer_hand)
-    puts dealer.value(dealer_total_value,dealer_hand)
-    player_selection
-    player_hit_card
-    dealer_hit_card
-    compare
-  end 
   def player_hit_card
     while player_total_value < 21 && selection == "hit"
       card = card_selection
@@ -65,28 +37,70 @@ class BlackJack
       puts "your hand is #{player_hand}"
       puts "your hands valus = #{player_total_value}"
       if player_total_value > 21 
-        compare
+        break
       end
       player_selection
     end 
   end 
+
   def player_selection
     puts "please select hit or stay"
     self.selection = gets.chomp.downcase
   end 
+end 
+
+
+class Dealer
+  include Cards
+  attr_accessor :dealer_hand, :dealer_total_value 
+  def initialize 
+    @dealer_hand = []
+    @dealer_total_value = 0
+  end 
+
+  def initial_cards
+    2.times{dealer_hand << card_selection}
+    "Dealer's hand is #{dealer_hand}"
+  end 
+
+  def value
+    dealer_hand.select{|x| self.dealer_total_value = dealer_total_value + card_value(x)}
+    "Dealers's hand value is #{dealer_total_value}"
+  end 
+
   def dealer_hit_card
     while dealer_total_value < 17 
       card = card_selection
       self.dealer_hand << card
       self.dealer_total_value = dealer_total_value + card_value(card)
-      puts dealer_total_value
-      puts dealer_hand
+      puts "Dealr's hand is #{dealer_hand}"
+      puts "Dealers hands valus = #{dealer_total_value}"
     end 
   end 
+end 
+
+class BlackJack 
+  include Cards 
+
+  def start
+    puts "Please enter your name"
+    player_name = gets.chomp
+    player_one = Player.new(player_name)
+    dealer = Dealer.new
+    puts player_one.initial_cards
+    puts player_one.value
+    puts dealer.initial_cards
+    puts dealer.value
+    player_one.player_selection
+    player_one.player_hit_card
+    dealer.dealer_hit_card
+    compare
+  end 
+
   def compare
-    if player_total_value == 21 && dealer_total_value != 21 
+    if player_one.player_total_value == 21 && dealer.dealer_total_value != 21 
       puts "Blackjack, you won!"
-    elsif player_total_value ==21 && dealer_total_value == 21 
+    elsif player_total_value == 21 && dealer_total_value == 21 
       puts "Tie"
     elsif player_total_value > dealer_total_value && player_total_value < 21 
       puts "You Won!"
